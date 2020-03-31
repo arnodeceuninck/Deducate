12a85eb8efa31a377769eb25a55e203bc3307cb7
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-03-31 21:33:08 +0000

    Updated changes on wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from app import app
-# import app
-if __name__ == "__main__":
-	app.run()
+from app import create_app, db
+from app.models import *
+
+app = create_app()
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db}
+
